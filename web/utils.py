b5579ada2799ec461575148217fa66f650539952
Hana Zivna <hanka.zajicova@gmail.com>
2025-03-12 16:31:40 +0100

    Improve readability

--- web/utils.py
+++ web/utils.py
@@ -1,8 +1,13 @@
+import logging
 from typing import Optional
+
+from rest_framework.response import Response
+
+logger = logging.getLogger(__name__)
 
 
 def _get_value_from_request(request, key, default=None):
-	return request.GET.get(key, default)
+    return request.GET.get(key, default)
 
 
 def _is_valid_lat_lon(lat: Optional[float], lon: Optional[float]) -> bool:
@@ -10,20 +15,23 @@ def _is_valid_lat_lon(lat: Optional[float], lon: Optional[float]) -> bool:
 
 
 def _check_lat_lon(lat: Optional[float], lon: Optional[float]) -> tuple[bool, Optional[str]]:
-	if lat is None and lon is None:
-		return False, None
+    if lat is None and lon is None:
+        return False, None
 
-	if lat is None or lon is None:
-		return False, "Please provide both 'lat' and 'lon'"
+    if lat is None or lon is None or not _is_valid_lat_lon(lat, lon):
+        return False, "Please provide both valid 'lat' and 'lon'"
 
-	if not _is_valid_lat_lon(lat, lon):
-		return False, "Please provide valid 'lat' and 'lon'"
-
-	return True, None
+    return True, None
 
 
 def _try_float_or_none(value: str) -> Optional[float]:
-	try:
-		return float(value) if value else None
-	except ValueError:
-		return None
+    try:
+        return float(value) if value else None
+    except ValueError:
+        return None
+
+
+def _handle_error_response(error_message: str) -> Response:
+    logger.error(error_message)
+
+    return Response({'error': error_message}, status=400)
--- web/views.py
+++ web/views.py
@@ -9,47 +9,89 @@ from rest_framework.response import Response
 
 from .models import TimezoneGeneral, TimezoneShape
 from .serializers import TimezoneSerializer
-from .utils import _check_lat_lon, _get_value_from_request, _try_float_or_none
+from .utils import (
+    _check_lat_lon,
+    _get_value_from_request,
+    _handle_error_response,
+    _try_float_or_none,
+)
 
 logger = logging.getLogger(__name__)
 
 
 class TimezoneViewSet(viewsets.ViewSet):
     UNINHABITED = 'uninhabited'
-    TERITORIAL_SEE_RADIUS = 12  # nautical miles
+    TERRITORIAL_SEA_RADIUS = 12  # nautical miles
 
     def list(self, request):
-        lat = _try_float_or_none(_get_value_from_request(request, 'lat'))
-        lon = _try_float_or_none(_get_value_from_request(request, 'lon'))
-        is_valid, message = _check_lat_lon(lat, lon)
-        if message:
-            return Response({'error': message}, status=400)
+        lat, lon = self._get_lat_lon_from_request(request)
+        is_valid, error_message = _check_lat_lon(lat, lon)
+
+        if error_message:
+            return _handle_error_response(error_message)
 
         if not is_valid:
-            queryset = list(
-                itertools.chain(
-                    TimezoneShape.objects.exclude(name=self.UNINHABITED).distinct('name'),
-                    TimezoneGeneral.objects.all(),
-                )
-            )
-            serializer = TimezoneSerializer(queryset, many=True)
-            return Response(serializer.data)
+            return self._list_all_timezones_response()
 
+        return self._list_single_timezone_response(lat, lon)
+
+    def _get_lat_lon_from_request(self, request) -> tuple[float | None, float | None]:
+        lat = _try_float_or_none(_get_value_from_request(request, 'lat'))
+        lon = _try_float_or_none(_get_value_from_request(request, 'lon'))
+
+        return lat, lon
+
+    def _list_all_timezones_response(self) -> Response:
+        queryset = self._list_all_timezones()
+        serializer = TimezoneSerializer(queryset, many=True)
+
+        return Response(serializer.data)
+
+    def _list_single_timezone_response(self, lat: float, lon: float) -> Response:
+        item, location_message = self._get_single_timezone(lat, lon)
+        if not item:
+            return _handle_error_response('Timezone not found for the provided location')
+
+        logger.info(
+            f'Location at lat: {lat}, lon: {lon} is {location_message} within the {item.name} timezone'
+        )
+
+        serializer = TimezoneSerializer(item)
+        return Response(serializer.data)
+
+    def _list_all_timezones(self) -> list:
+        items = list(
+            itertools.chain(
+                TimezoneShape.objects.exclude(name=self.UNINHABITED).distinct('name'),
+                TimezoneGeneral.objects.all(),
+            )
+        )
+
+        return items
+
+    def _get_single_timezone(
+        self, lat: float, lon: float
+    ) -> tuple[TimezoneGeneral | TimezoneShape, str]:
         point = GEOSGeometry(f'POINT({lon} {lat})', srid=4326)
 
-        # Check if the point is in the shapefile or within the territorial waters
+        # Check if the point is in the shapefile or within the territorial sea
         item = (
-            TimezoneShape.objects.exclude(name=self.UNINHABITED)
-            .filter(poly__distance_lte=(point, D(nm=self.TERITORIAL_SEE_RADIUS)))
+            TimezoneShape.objects.filter(
+                poly__distance_lte=(point, D(nm=self.TERRITORIAL_SEA_RADIUS))
+            )
             .annotate(distance=Distance('poly', point))
             .order_by('distance')
             .first()
         )
 
-        if not item:
-            item = TimezoneGeneral.objects.filter(long_min__lte=lon, long_max__gte=lon).first()
-            if not item:
-                return Response({'error': 'Invalid longtitude'}, status=400)
+        location_message = 'in territorial sea' if item and item.distance.m > 0 else 'on land'
+        is_uninhabited = item.name == self.UNINHABITED if item else False
 
-        serializer = TimezoneSerializer(item)
-        return Response(serializer.data)
+        if not item or is_uninhabited:
+            item = TimezoneGeneral.objects.filter(long_min__lte=lon, long_max__gte=lon).first()
+
+            location_message = (
+                'in international waters' if not is_uninhabited else 'in an uninhabited area'
+            )
+
+        return item, location_message
