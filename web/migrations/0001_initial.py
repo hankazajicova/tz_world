5337652162c0e5a1effec3ec762fd0ba7a0086cf
Hana Zivna <hanka.zajicova@gmail.com>
2025-03-12 15:17:01 +0100

    Remove logging handler

--- web/migrations/0001_initial.py
+++ web/migrations/0001_initial.py
@@ -13,13 +13,7 @@ from django.db import migrations, models
 import logging
 
 
-# Set up logging
 logger = logging.getLogger(__name__)
-# logger.setLevel(logging.INFO)
-# handler = logging.StreamHandler()
-# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-# handler.setFormatter(formatter)
-# logger.addHandler(handler)
 
 
 def get_general_timezones(apps, schema_editor):
