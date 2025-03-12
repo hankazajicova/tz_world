913d1ea4ede170ae407f244ca018cd45dad6fbab
Hana Zivna <hanka.zajicova@gmail.com>
2025-03-12 12:40:00 +0100

    Add logging

--- get_tz_app/settings.py
+++ get_tz_app/settings.py
@@ -88,6 +88,31 @@ DATABASES = {
 }
 
 
+LOGGING = {
+    "version": 1,  # the dictConfig format version
+    "disable_existing_loggers": False,  # retain the default loggers
+    "formatters": {
+        "standard": {
+            "format": "[%(asctime)s] %(name)s %(funcName)s [%(levelname)s] - %(message)s",
+            "datefmt": "%d/%b/%Y %H:%M:%S",
+        },
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "standard",
+        },
+    },
+    "loggers": {
+        "": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+    },
+}
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
